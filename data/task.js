4d5a0a839ddcf7d04765cedfff95a8cd476eab2a
heiweigou <jiaowangzhen@hotmail.com>
2017-12-06 12:56:26 +1100

    Initial commit

---
+++ data/task.js
@@ -0,0 +1,6 @@
+var tasks=[
+    {"id":100,"task":"AJAX test task 1","requiredBy":"2017-12-01","category":"Personal","complete": false},
+    {"id":101,"task":"AJAX test task 2","requiredBy":"2017-12-02","category":"Work","complete": true},
+    {"id":102,"task":"AJAX test task 3","requiredBy":"2017-12-03","category":"Work","complete": false},
+    {"id":103,"task":"AJAX test task 4","requiredBy":"2017-12-04","category":"Personal","complete": false}
+]
